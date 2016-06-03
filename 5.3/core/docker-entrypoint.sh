f38cfaddb69fa4967b09e7ebc8cdff651d5ad203
TehesFR <ths.arborisoft@gmail.com>
2016-06-03 18:23:48 +0200

    fix permission issue in /var/www

--- 5.3/core/docker-entrypoint.sh
+++ 5.3/core/docker-entrypoint.sh
@@ -45,5 +45,6 @@ sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/sites-available/default
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
 
+chown -R www-data:www-data /var/www/
 # Start Apache in foreground
 source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND
--- 7.0/core/docker-entrypoint.sh
+++ 7.0/core/docker-entrypoint.sh
@@ -27,5 +27,6 @@ sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/apache2.conf
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
 
+chown -R www-data:www-data /var/www/
 # Start Apache in foreground
 /usr/sbin/apache2 -DFOREGROUND
