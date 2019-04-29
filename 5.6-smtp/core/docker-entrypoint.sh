4b2e9f94f8ccf39fa4de527018707e9832cd6f52
franckdrion <drionfranck@gmail.com>
2019-04-29 15:42:22 +0200

    smtpconf for prod env.

---
+++ 5.6-smtp/core/docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+set -e
+
+# Configure msmtp
+sed -i "s/MY_SMTP/$SMTP/g" /usr/local/etc/msmtprc
+sed -i "s/MY_EMAIL_DOMAIN/$EMAIL_DOMAIN/g" /usr/local/etc/msmtprc
+sed -i "s/MY_OUTGOING_ADDRESS/$OUTGOING_ADDRESS/g" /usr/local/etc/msmtprc
+
+
+# set msmtp permissions
+chmod 600 /usr/local/etc/msmtprc && chown web /usr/local/etc/msmtprc
+
+# Set the apache user and group to match the host user.
+# This script will change the www-data UID/GID in the container from to 33 (default) to the UID/GID of the host user, if the current host user is not root.
+OWNER=$(stat -c '%u' /var/www/html)
+GROUP=$(stat -c '%g' /var/www/html)
+USERNAME=www-data
+[ -e "/etc/debian_version" ] || USERNAME=apache
+if [ "$OWNER" != "0" ]; then
+  usermod -o -u $OWNER $USERNAME
+  usermod -s /bin/bash $USERNAME
+  groupmod -o -g $GROUP $USERNAME
+  usermod -d /var/www/html $USERNAME
+  chown -R --silent $USERNAME:$USERNAME /var/www/html
+fi
+echo The apache user and group has been set to the following:
+id $USERNAME
+
+usermod -d /var/www www-data
+
+# Apache2 custom servername, alias and documentroot
+sed -i "s/MYSERVERNAME/$SERVERNAME/g" /etc/apache2/apache2.conf
+sed -i "s/MYSERVERALIAS/$SERVERALIAS/g" /etc/apache2/apache2.conf
+sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/apache2.conf
+
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
+# Start Apache in foreground
+/usr/sbin/apache2 -DFOREGROUND
